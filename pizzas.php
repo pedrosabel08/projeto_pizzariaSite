b7f6182bcaa709583df0633dd7ca72e9c078bbdf
pedrosabel08 <110993286+pedrosabel08@users.noreply.github.com>
2024-05-13 21:23:01 -0300

    Add files via upload

---
+++ pizzas.php
@@ -0,0 +1,29 @@
+<?php
+include 'conexao.php';
+
+$sql = "SELECT * FROM pizzas";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Pizzas</h2>
+
+</body>
+</html>
