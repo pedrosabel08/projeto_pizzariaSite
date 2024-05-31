0467aa8ec3d3e93b10e687e1f8db19c387835f3c
Pedro Sabel <pedrosabel08@gmail.com>
2024-05-30 21:56:22 -0300

    criado HTML e CSS para o menu principal da parte de vendas

---
+++ Vendas/menuVendas.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleMenuVendas.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div class="main-container">
+            <ul>
+                <img src="img/domJoao.jpg" alt="logo" class="logo">
+                <li>
+                    <h1>PIZZARIA DOM JOÃO</h1>
+                </li>
+                <li>
+                    <a href="https://www.google.com/maps/search/?api=1&query=%27Rua%20Jo%C3%A3o%20Pessoa,%201726%20Sl%2004%20-%20Velha%20-%20Blumenau%20/%20SC%27"
+                        target="blink">Rua João Pessoa, 1726 Sl 04 - Velha - Blumenau/SC</a>
+                </li>
+                <li>
+                    <img src="img/pizzas.jpg" alt="pizza" class="pizza">
+                </li>
+            </ul>
+        </div>
+        <div class="pizzas-container">
+            <h1>Pizzas</h1>
+            <ul>
+                <li>
+                    <img src="img/pizzaLove.jpg" alt="Pizza Love">
+                    <div class="description">
+                        <h2>PIZZA LOVE</h2>
+                        <p>PIZZA EM FORMATO DE CORAÇÃO, MISTA CHOCOLATE BRANCO E PRETO</p>
+                        <p class="price">A partir de R$ 20,00</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/pizzaBaby.jpg" alt="Pizza Baby">
+                    <div class="description">
+                        <h2>PIZZA BABY</h2>
+                        <p>20cm, 4 fatias, 1 sabor</p>
+                        <p class="price">A partir de R$ 29,90</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/pizzaMedia.jpg" alt="Pizza Média">
+                    <div class="description">
+                        <h2>PIZZA MÉDIA</h2>
+                        <p>30cm, 8 fatias, 2 sabores</p>
+                        <p class="price">A partir de R$ 58,90</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/pizzaGrande.jpg" alt="Pizza Grande">
+                    <div class="description">
+                        <h2>PIZZA Grande</h2>
+                        <p>35cm, 12 fatias, 3 sabores</p>
+                        <p class="price">A partir de R$ 76,90</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/pizzaGigante.jpg" alt="Pizza Gigante">
+                    <div class="description">
+                        <h2>PIZZA MÉDIA</h2>
+                        <p>45cm, 16 fatias, 4 sabores</p>
+                        <p class="price">A partir de R$ 87,90</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
+</body>
+
+</html>
