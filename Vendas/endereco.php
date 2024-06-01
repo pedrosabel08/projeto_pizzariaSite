ca5619c6f583cac740af3d993b7cd978a339d8f2
Pedro Sabel <pedrosabel08@gmail.com>
2024-06-01 00:14:00 -0300

    cadastro de endereço para o cliente

---
+++ Vendas/endereco.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro de Endereço</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="wrapper">
+    <div class="form-wrapper sign-up">
+      <form action="cadastro_endereco.php" method="POST">
+        <h2>Cadastro de Endereço</h2>
+        <input type="hidden" name="cliente_id" value="<?php echo $_GET['cliente_id']; ?>">
+        <div class="input-group">
+          <input type="text" name="logradouro" required>
+          <label for="logradouro">Logradouro</label>
+        </div>
+        <div class="input-group">
+          <input type="text" name="numero" required>
+          <label for="numero">Número</label>
+        </div>
+        <div class="input-group">
+          <input type="text" name="complemento">
+          <label for="complemento">Complemento</label>
+        </div>
+        <div class="input-group">
+          <input type="text" name="bairro" required>
+          <label for="bairro">Bairro</label>
+        </div>
+        <div class="input-group">
+          <input type="text" name="cidade" required>
+          <label for="cidade">Cidade</label>
+        </div>
+        <button type="submit">Cadastrar Endereço</button>
+      </form>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
